de02621f94e01e28ea0ff0f8351f3c971a27c675
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-28 03:17:00 +0400

    Add SDL example code, simple CMake file and .gitignore.
    Update README.md as well.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 Implementing CHIP-8 emulator because I want to know how it works and also, it is fun.
 By all means this is not supposed to be the most efficient implementation.
+
+
+
+### Issues with running SDL2
+#### MacOS
+       
+    When building SDL from source, you have probably got this issue after running the binary:
+
+        dyld: Library not loaded: PATH_TO_LIB/libSDL2-2.0.0.dylib
+        Referenced from: ./your_binary
+        Reason: image not found
+
+    To resolve this you can create symbolic link to libSDL2-2.0.0.dylib in /usr/local/lib
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,45 @@
-int main() {
-  return 0;
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        printf("%s %s\n", "Could not initialize SDL! Error code: ", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("CHIP-8 Emulator",
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SCREEN_WIDTH,
+                                  SCREEN_HEIGHT,
+                                  SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("%s %s\n", "Could not create SDL window! Error code: ", SDL_GetError());
+        } else {
+            SDL_Surface* screen_surface = SDL_GetWindowSurface(window);
+            if (screen_surface == NULL) {
+                printf("%s %s\n", "Could not get window surface! Error code: ", SDL_GetError());
+            } else {
+                SDL_Event event;
+                int is_running = 1;
+                while (is_running) {
+                    while (SDL_PollEvent(&event)) {
+                        if (event.type == SDL_QUIT) {
+                            is_running = 0;
+                        }
+                    }
+                    SDL_FillRect(screen_surface, NULL,
+                                 SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+                    SDL_UpdateWindowSurface(window);
+                }
+            }
+        }
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    return 0;
 }
